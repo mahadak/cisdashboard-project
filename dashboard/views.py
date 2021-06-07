e5dfebf862f45cae0efcc0eeb326b00527895fc2
Mahad Akbar <mahad.akbar@invozone.com>
2021-06-07 19:25:17 +0500

    change path

--- dashboard/views.py
+++ dashboard/views.py
@@ -555,7 +555,7 @@ class IamPolicyGraphicalView(View):
             s3 = boto3.client('s3', **AWS_CREDS)
             s3.upload_file(local_image, 'cisbucket2021', 'graph.png')
             url = "https://cisbucket2021.s3.amazonaws.com/graph.png"
-            webbrowser.open(url)
+            webbrowser.open_new_tab(url)
             print(url)
             context = {"message": True}
         except Exception as e:
