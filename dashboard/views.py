1e2c6f8a89b32c38b97bd2f14ac5c481a11e1c39
Mahad Akbar <mahad.akbar@invozone.com>
2021-06-07 19:29:22 +0500

    fixes

--- dashboard/views.py
+++ dashboard/views.py
@@ -555,8 +555,7 @@ class IamPolicyGraphicalView(View):
             s3 = boto3.client('s3', **AWS_CREDS)
             s3.upload_file(local_image, 'cisbucket2021', 'graph.png')
             url = "https://cisbucket2021.s3.amazonaws.com/graph.png"
-            webbrowser.open_new_tab(url)
-            print(url)
+            webbrowser.open(url, new=2, autoraise=True)
             context = {"message": True}
         except Exception as e:
             context = {'message': str(e)}
